cb3c3c62e55d83c716deac4d7475ae0c21f3426c
Antoine Boucher <antoine.bou13@gmail.com>
2023-04-09 13:09:32 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,46 +4,69 @@ from rich.console import Console
 console = Console()
 
 # Define constants
-MONTHLY_MESSAGES = 25 * 24 * 30  # 25 messages every 3 hours for 30 days
 TOKENS_PER_K_WORDS = 1000 / 750  # 1k tokens is equivalent to ~750 words
-GPT4_PRICE_PER_TOKEN_PROMPT = 0.03  # GPT-4 price per 1k prompt request tokens
-GPT4_PRICE_PER_TOKEN_COMPLETION = 0.06  # GPT-4 price per 1k completion response tokens
-GPT35_TURBO_PRICE_PER_TOKEN = 0.002  # GPT-3.5-turbo price per token
-LOWER_BOUND_PROMPT_SIZE = 50
-UPPER_BOUND_PROMPT_SIZE = 200
 
 # Define functions to calculate cost
-def calculate_cost_gpt4_8k(prompts_per_month, prompt_size, price_per_token_prompt, price_per_token_completion):
-    tokens_per_prompt = prompt_size * TOKENS_PER_K_WORDS
-    tokens_per_completion = tokens_per_prompt
-    return (prompts_per_month * tokens_per_prompt * price_per_token_prompt
-            + prompts_per_month * tokens_per_completion * price_per_token_completion) / 1000
-
-def calculate_cost_gpt4_32k(prompts_per_month, prompt_size, price_per_token_prompt, price_per_token_completion):
-    tokens_per_prompt = prompt_size * TOKENS_PER_K_WORDS
-    tokens_per_completion = tokens_per_prompt * 4  # GPT-4 32K has 4x the completion response tokens
-    return (prompts_per_month * tokens_per_prompt * price_per_token_prompt
-            + prompts_per_month * tokens_per_completion * price_per_token_completion) / 1000
-
-def calculate_cost_gpt35_turbo(prompts_per_month, prompt_size, price_per_token):
-    tokens_per_prompt = prompt_size * TOKENS_PER_K_WORDS
-    return prompts_per_month * tokens_per_prompt * price_per_token / 1000
+def calculate_cost(model, prompt_size, tokens_per_month):
+    if model == "gpt4_8k":
+        price_per_token_prompt = 0.03
+        price_per_token_completion = 0.06
+        tokens_per_prompt = prompt_size * TOKENS_PER_K_WORDS
+        tokens_per_completion = tokens_per_prompt
+        cost = (tokens_per_month * tokens_per_prompt * price_per_token_prompt
+                + tokens_per_month * tokens_per_completion * price_per_token_completion) / 1000
+    elif model == "gpt4_32k":
+        price_per_token_prompt = 0.06
+        price_per_token_completion = 0.12
+        tokens_per_prompt = prompt_size * TOKENS_PER_K_WORDS
+        tokens_per_completion = tokens_per_prompt * 4  # GPT-4 32K has 4x the completion response tokens
+        cost = (tokens_per_month * tokens_per_prompt * price_per_token_prompt
+                + tokens_per_month * tokens_per_completion * price_per_token_completion) / 1000
+    elif model == "chat_gpt":
+        price_per_token = 0.002
+        tokens_per_prompt = prompt_size * TOKENS_PER_K_WORDS
+        cost = tokens_per_month * tokens_per_prompt * price_per_token / 1000
+    elif model in ["ada", "babbage", "curie", "davinci"]:
+        if model == "ada":
+            price_per_training_token = 0.0004
+            price_per_usage_token = 0.0016
+        elif model == "babbage":
+            price_per_training_token = 0.0006
+            price_per_usage_token = 0.0024
+        elif model == "curie":
+            price_per_training_token = 0.0030
+            price_per_usage_token = 0.0120
+        elif model == "davinci":
+            price_per_training_token = 0.0300
+            price_per_usage_token = 0.1200
+        cost = tokens_per_month * price_per_usage_token / 1000
+    elif model == "embedding_ada":
+        price_per_token = 0.0004
+        cost = tokens_per_month * price_per_token / 1000
+    elif model == "embedding_curie":
+        price_per_token = 0.0006
+        cost = tokens_per_month * price_per_token / 1000
+    elif model == "image_1024":
+        price_per_image = 0.020
+        cost = price_per_image * tokens_per_month
+    elif model == "image_512":
+        price_per_image = 0.018
+        cost = price_per_image * tokens_per_month
+    elif model == "image_256":
+        price_per_image = 0.016
+        cost = price_per_image * tokens_per_month
+    elif model == "whisper":
+        price_per_minute = 0.006
+        cost = price_per_minute * tokens_per_month / 60
+    else:
+        console.print("Invalid model. Please choose a valid model.", style="bold red")
+        return None
+    return cost
 
 # Create CLI app with typer
 app = typer.Typer()
 
 @app.command()
-def calculate_costs(lower_bound_prompt_size: int = LOWER_BOUND_PROMPT_SIZE, upper_bound_prompt_size: int = UPPER_BOUND_PROMPT_SIZE):
-    # Calculate costs for lower bound of average prompt size (50 tokens per message)
-    gpt4_8k_lower_bound_cost = calculate_cost_gpt4_8k(MONTHLY_MESSAGES, lower_bound_prompt_size,
-                                                      GPT4_PRICE_PER_TOKEN_PROMPT, GPT4_PRICE_PER_TOKEN_COMPLETION)
-    gpt4_32k_lower_bound_cost = calculate_cost_gpt4_32k(MONTHLY_MESSAGES, lower_bound_prompt_size,
-                                                        GPT4_PRICE_PER_TOKEN_PROMPT, GPT4_PRICE_PER_TOKEN_COMPLETION)
-    gpt35_turbo_lower_bound_cost = calculate_cost_gpt35_turbo(MONTHLY_MESSAGES, lower_bound_prompt_size,
-                                                              GPT35_TURBO_PRICE_PER_TOKEN)
-
-    # Calculate costs for upper bound of average prompt size (200 tokens per message)
-    gpt4_8k_upper_bound_cost = calculate_cost_gpt4_8k(MONTHLY_MESSAGES, upper_bound_prompt_size,
-                                                      GPT4_PRICE_PER_TOKEN_PROMPT, GPT4_PRICE_PER_TOKEN_COMPLETION)
-    gpt4_32k_upper_bound_cost = calculate_cost_gpt4_32k(MONTHLY_MESSAGES, upper_bound_prompt_size,
-                                                        GPT4_PRICE_PER_TOKEN_PROMPT, GPT4_PRICE_PER_TOKEN_COMPLETION)
+def calculate_costs(model: str, prompt_size: int, messages_per_day: int):
+    tokens_per_month
+    
